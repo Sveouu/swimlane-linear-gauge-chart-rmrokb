b1f061874bcd97e75692d2aaf3404cfdde07bba1
Sveouu <72437441+Sveouu@users.noreply.github.com>
2023-08-24 13:47:28 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# swimlane-linear-gauge-chart-rmrokb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/swimlane-linear-gauge-chart-rmrokb)
---
+++ app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  single: any[];
+  view: any[] = [400, 400];
+  colorScheme = {
+    domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5']
+  };
+  value: number = 50;
+  previousValue: number = 70;
+  units: string = 'counts';
+
+  onSelect(event) {
+    console.log(event);
+  }
+}
